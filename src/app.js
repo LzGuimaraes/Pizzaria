1349e2abae1d23bb00a518d03937c66036ff1fbf
Luiz <131723272+LzGuimaraes@users.noreply.github.com>
2026-05-24 12:31:18 -0400

    Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://pizzaria-hymw.onrender.com/',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
